490cfbc58605ff0f77e304aa4cb949f1d0d92158
Stijn <39174471+aeiodelic@users.noreply.github.com>
2023-03-07 20:04:02 +0100

    Update speakerplug.py

--- speakerplug.py
+++ speakerplug.py
@@ -2,7 +2,7 @@ import json
 import sys
 from PyP100 import PyP100
 
-config = json.load(open('config.json'))
+config = json.load(open('C:\speakerplug\config.json'))
 
 plug = PyP100.P100(config['ip'], config['email'], config['password'])
 
